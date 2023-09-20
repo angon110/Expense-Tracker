9a906c9e0e16af747343b00ac51239ab8c2a4b1c
angon110 <angon150ca@gmail.com>
2023-09-19 20:09:39 -0400

    Added Request for User Budget

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,38 +1,46 @@
-# Import the Expense class from the expense module.
+# Import necessary modules
 from expense import Expense
-
-# Import the calendar and datetime modules for date-related calculations.
 import calendar
 import datetime
 
-# Define the main function to run the Expense Tracker.
+# Define the main function
 def main():
+    # Display a welcome message
     print(f"🎯 Running Expense Tracker!")
 
-    # Specify the file path where expense data will be stored.
+    # Specify the file path to store expense data
     expense_file_path = "expenses.csv"
 
-    # Set the budget for expenses.
-    budget = 2000
+    # Get the initial budget from the user
+    budget = get_initial_budget()
 
-    # Get user input for a new expense.
+    # Get user input for a new expense
     expense = get_user_expense()
 
-    # Save the user's expense to the specified file.
+    # Write the new expense to a file
     save_expense_to_file(expense, expense_file_path)
 
-    # Read and summarize expenses from the file.
+    # Read the file and summarize expenses
     summarize_expenses(expense_file_path, budget)
 
-# Function to get user input for a new expense.
+# Function to get and validate the user's initial budget
+def get_initial_budget():
+    print(f"🎯 Getting Initial Budget")
+    while True:
+        try:
+            initial_budget = float(input("Enter your budget: $"))
+            if initial_budget >= 0:
+                return initial_budget
+            else:
+                print("Budget must be a positive number. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid numeric value for your budget.")
+
+# Function to get user input for a new expense
 def get_user_expense():
     print(f"🎯 Getting User Expense")
-
-    # Prompt the user to enter expense details.
     expense_name = input("Enter expense name: ")
     expense_amount = float(input("Enter expense amount: "))
-
-    # Define expense categories.
     expense_categories = [
         "🍔 Food",
         "🏠 Home",
@@ -42,7 +50,6 @@ def get_user_expense():
     ]
 
     while True:
-        # Display available categories and allow the user to select one.
         print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
             print(f"  {i + 1}. {category_name}")
@@ -52,8 +59,6 @@ def get_user_expense():
 
         if selected_index in range(len(expense_categories)):
             selected_category = expense_categories[selected_index]
-
-            # Create a new Expense object and return it.
             new_expense = Expense(
                 name=expense_name, category=selected_category, amount=expense_amount
             )
@@ -61,18 +66,16 @@ def get_user_expense():
         else:
             print("Invalid category. Please try again!")
 
-# Function to save an expense to a file.
+# Function to save an expense to a file
 def save_expense_to_file(expense: Expense, expense_file_path):
     print(f"🎯 Saving User Expense: {expense} to {expense_file_path}")
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
 
-# Function to read and summarize expenses from a file.
+# Function to summarize expenses and display the results
 def summarize_expenses(expense_file_path, budget):
     print(f"🎯 Summarizing User Expense")
     expenses: list[Expense] = []
-
-    # Read expense data from the file and create Expense objects.
     with open(expense_file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
@@ -84,7 +87,7 @@ def summarize_expenses(expense_file_path, budget):
             )
             expenses.append(line_expense)
 
-    # Calculate and display expenses by category.
+    # Calculate expenses by category
     amount_by_category = {}
     for expense in expenses:
         key = expense.category
@@ -93,28 +96,31 @@ def summarize_expenses(expense_file_path, budget):
         else:
             amount_by_category[key] = expense.amount
 
+    # Display expenses by category
     print("Expenses By Category 📈:")
     for key, amount in amount_by_category.items():
         print(f"  {key}: ${amount:.2f}")
 
-    # Calculate and display total spent, remaining budget, and daily budget.
+    # Calculate total spent
     total_spent = sum([x.amount for x in expenses])
     print(f"💵 Total Spent: ${total_spent:.2f}")
 
+    # Calculate remaining budget
     remaining_budget = budget - total_spent
     print(f"✅ Budget Remaining: ${remaining_budget:.2f}")
 
+    # Calculate daily budget
     now = datetime.datetime.now()
     days_in_month = calendar.monthrange(now.year, now.month)[1]
     remaining_days = days_in_month - now.day
-
     daily_budget = remaining_budget / remaining_days
     print(green(f"👉 Budget Per Day: ${daily_budget:.2f}"))
 
-# Function to add green color to text for console output.
+# Function to add green color to text for console output
 def green(text):
     return f"\033[92m{text}\033[0m"
 
-# Check if the script is executed as the main program.
+# Check if the script is executed as the main program
 if __name__ == "__main__":
     main()
+
